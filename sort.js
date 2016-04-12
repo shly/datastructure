84bb8bddea9e396b7f01a8efaa58d4fd8c41f7b4
sly <shanglingyue1992@163.com>
2016-04-12 11:02:01 +0800

    quikSort

--- sort.js
+++ sort.js
@@ -33,6 +33,29 @@ function insertSort(){
 	}
 	document.querySelector(".right").innerHTML = "插入排序结果： "+arr.toString();
 }
+function subQuikSort(arr,start,end){
+	if(end>start){
+		var temp = arr[start],
+		    i = start,
+		    j = end+1;
+		while(true){
+			while(i<end&&arr[++i]<temp);
+			while(j>start&&arr[--j]>temp);
+			if(i<j){
+				swap(arr,i,j);
+			}else{
+				break;
+			}
+		}
+		swap(arr,start,j);
+		subQuikSort(arr,start,j-1);
+		subQuikSort(arr,j+1,end);
+	}
+	document.querySelector(".right").innerHTML = "快速排序结果： "+arr.toString();
+}
+function quikSort(){
+	return subQuikSort(arr,0,arr.length-1);
+}
 function swap(arr,i,j){
 	var temp = arr[i];
 	arr[i]=arr[j];
@@ -42,3 +65,4 @@ function swap(arr,i,j){
 EventUtil.addEvent(document.querySelector(".bubbleSort"),"click",bubbleSort);
 EventUtil.addEvent(document.querySelector(".selectSort"),"click",selectSort);
 EventUtil.addEvent(document.querySelector(".insertSort"),"click",insertSort);
+EventUtil.addEvent(document.querySelector(".quikSort"),"click",quikSort);

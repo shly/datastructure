4b776d96e41ed2b78d9a4a9bff933ca73de949b0
sly <shanglingyue1992@163.com>
2016-04-12 09:57:10 +0800

    selectSort

--- sort.js
+++ sort.js
@@ -1,14 +1,26 @@
 var arr = [10,9,19,8,23,7,3];
 document.querySelector(".left").innerHTML = arr.toString();
 function bubbleSort(){
-	for (var i = arr.length - 1; i >= 1; i--) {
+	for (var i = arr.length-1; i > 0; i--) {
 		for(var j= 0;j<i;j++){
 			if(arr[j]>arr[j+1]){
 				swap(arr,j,j+1);
 			}
 		}
 	}
-	document.querySelector(".right").innerHTML = arr.toString();
+	document.querySelector(".right").innerHTML = "冒泡排序结果： "+arr.toString();
+}
+function selectSort(){
+	for (var i = 0; i < arr.length; i++) {
+		var min = i;
+		for(var j= i+1;j<arr.length;j++){
+			if(arr[min]>arr[j]){
+				min = j;
+			}
+		}
+		swap(arr,i,min);
+	}
+	document.querySelector(".right").innerHTML = "选择排序结果： "+arr.toString();
 }
 function swap(arr,i,j){
 	var temp = arr[i];
@@ -16,3 +28,4 @@ function swap(arr,i,j){
 	arr[j] = temp;
 }
 EventUtil.addEvent(document.querySelector(".bubbleSort"),"click",bubbleSort);
+EventUtil.addEvent(document.querySelector(".selectSort"),"click",selectSort);

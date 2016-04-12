5039eb41a58b0350c99fcfd32aea469992c7d250
sly <shanglingyue1992@163.com>
2016-04-12 09:37:54 +0800

    bubbleSort

--- sort.js
+++ sort.js
@@ -1,6 +1,6 @@
-var arr = [03,13,19,20,23,26,03];
+var arr = [10,9,19,8,23,7,3];
 document.querySelector(".left").innerHTML = arr.toString();
-function bubbleSort(arr){
+function bubbleSort(){
 	for (var i = arr.length - 1; i >= 1; i--) {
 		for(var j= 0;j<i;j++){
 			if(arr[j]>arr[j+1]){
@@ -8,10 +8,11 @@ function bubbleSort(arr){
 			}
 		}
 	}
-
+	document.querySelector(".right").innerHTML = arr.toString();
 }
 function swap(arr,i,j){
 	var temp = arr[i];
 	arr[i]=arr[j];
 	arr[j] = temp;
 }
+EventUtil.addEvent(document.querySelector(".bubbleSort"),"click",bubbleSort);
